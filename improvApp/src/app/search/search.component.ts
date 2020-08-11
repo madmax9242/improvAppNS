ea8085cc2881b2384f9ad693ecedce7f3d5b8d71
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-08-11 18:27:44 -0400

    added setting/activity calls

--- improvApp/src/app/search/search.component.ts
+++ improvApp/src/app/search/search.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { StarWarsCharacter } from "../classes/star-wars-character";
-import { StarWarsService } from '../services/star-wars.service';
+import { ScenarioService } from '../services/scenario.service';
 import { PlayerCharacter } from "../classes/playerCharacter";
+import { Activity } from "../classes/activity";
+import { Setting } from "../classes/setting";
 
 @Component({
     selector: "Search",
@@ -9,20 +11,33 @@ import { PlayerCharacter } from "../classes/playerCharacter";
 })
 export class SearchComponent implements OnInit {
 
-    character: StarWarsCharacter;
-    character2: StarWarsCharacter;
+    listOfActivities: Activity[];
+    listOfSettings: Setting[];
     listOfCharacters: PlayerCharacter[];
 
 
-    constructor(private swService: StarWarsService) {
+    constructor(private scenarioService: ScenarioService) {
         // Use the constructor to inject services.
     }
 
     ngOnInit(): void {
         // Use the "ngOnInit" handler to initialize data for the view.
-        this.swService.getAllCharacters().subscribe(result => {
+        this.scenarioService.getAllCharacters().subscribe(result => {
             this.listOfCharacters = result;
             console.log(this.listOfCharacters[2]);
+            console.log(this.listOfCharacters[5]);
         });
+
+        this.scenarioService.getAllActivities().subscribe(result => {
+            this.listOfActivities = result;
+            console.log(this.listOfActivities[1]);
+        });
+
+        this.scenarioService.getAllSettings().subscribe(result => {
+            this.listOfSettings = result;
+            console.log(this.listOfSettings[0]);
+        })
+
+        
     }
 }
