443708051d75a1405398e97dc318f087f2036826
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-08-13 14:02:57 -0400

    trying to add user input

--- improvApp/src/app/browse/browse.component.ts
+++ improvApp/src/app/browse/browse.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Setting } from "../classes/setting";
+import { Activity } from "../classes/activity";
+import { PlayerCharacter } from "../classes/playerCharacter";
 
 @Component({
     selector: "Browse",
     templateUrl: "./browse.component.html"
 })
 export class BrowseComponent implements OnInit {
+
+    characterFieldValue: string = "Hank";
+    settingFieldValue: string = "Outside";
+    activityFieldValue: string = "Playing catch";
     constructor() {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
         // Use the "ngOnInit" handler to initialize data for the view.
+    }
+
+    displayFieldValues() {
+        var characterFV = "Hank";
+        //this.characterFieldValue= "Henry";
+        //console.log(this.activityFieldValue);
     }
 }
--- improvApp/src/app/browse/browse.module.ts
+++ improvApp/src/app/browse/browse.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NativeScriptFormsModule } from 'nativescript-angular/forms'
 
 import { BrowseRoutingModule } from "./browse-routing.module";
 import { BrowseComponent } from "./browse.component";
@@ -7,6 +8,7 @@ import { BrowseComponent } from "./browse.component";
 @NgModule({
     imports: [
         NativeScriptCommonModule,
+        NativeScriptFormsModule,
         BrowseRoutingModule
     ],
     declarations: [
